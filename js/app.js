716735dea5671038ae8914fe42d81ed63c74aef8
LOVE\G2719 <2719562658@qq.com>
2026-06-22 22:18:51 +0800

    初始化

---
+++ js/app.js
@@ -0,0 +1,36 @@
+function downloadPackage(url, filename) {
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function renderPackages() {
+    const grid = document.getElementById("package-grid");
+
+    packages.forEach((pkg) => {
+        const card = document.createElement("div");
+        card.className = "package-card";
+
+        card.innerHTML = `
+            <div class="image-wrapper">
+                <img src="${pkg.image}" alt="${pkg.name}" loading="lazy" />
+            </div>
+            <div class="info">
+                <span class="name" data-file="${pkg.file}" data-name="${pkg.name}">${pkg.name}</span>
+            </div>
+        `;
+
+        card.querySelector(".name").addEventListener("click", (e) => {
+            const file = e.target.dataset.file;
+            const name = e.target.dataset.name;
+            downloadPackage(file, name);
+        });
+
+        grid.appendChild(card);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", renderPackages);
---
+++ js/data.js
@@ -0,0 +1,8 @@
+const packages = [
+    {
+        id: "dotween",
+        name: "Dotween.unitypackage",
+        image: "img/text.jpeg",
+        file: "resource/Dotween.unitypackage"
+    }
+];
